4327eb94d55d0b2f5022c47cc4bb4ee9523df7b8
Bradley Bown <bradbown@googlemail.com>
2021-09-27 22:39:08 +0100

    GenerateWallet() method created

--- BitcoinWallet/MainPage.xaml.cs
+++ BitcoinWallet/MainPage.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
+using NBitcoin;
 
 namespace BitcoinWallet
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        Wallet wallet;
 
         public MainPage()
         {
@@ -16,6 +18,12 @@ namespace BitcoinWallet
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
+        }
+
+        private void GenerateWallet()
+        {
+            wallet = new Wallet(NBitcoin.Network.Main, "walletDirectory");
+            wallet.GenerateWallet("walletName", "password");
         }
     }
 }
