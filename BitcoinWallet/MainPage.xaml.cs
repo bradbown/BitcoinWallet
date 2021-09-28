5d751f2ee76be1005bda65a396dbb44653db23d3
Bradley Bown <bradbown@googlemail.com>
2021-09-29 00:41:20 +0100

    Testing address types

--- BitcoinWallet/MainPage.xaml.cs
+++ BitcoinWallet/MainPage.xaml.cs
@@ -31,11 +31,14 @@ namespace BitcoinWallet
         {
             wallet = new Wallet(NBitcoin.Network.Main, "walletDirectory");
             keys = wallet.GenerateWallet("walletName", "password");
-            MnemonicLabel.Text = $"MnemonicLabel: {keys.mnemonic}";
-            ExtKeyLabel.Text = $"ExtKeyLabel: {keys.extKey}";
-            HDFingerprintLabel.Text = $"HDFingerprintLabel: {keys.masterKeyFingerprint}";
-            ExtPubKeyLabel.Text = $"ExtPubKeyLabel: {keys.extPubKey}";
-
+            MnemonicLabel.Text = $"Mnemonic: {keys.mnemonic}";
+            ExtKeyLabel.Text = $"ExtKey: {keys.extKey}";
+            HDFingerprintLabel.Text = $"HDFingerprint: {keys.masterKeyFingerprint}";
+            ExtPubKeyLabel.Text = $"ExtPubKey: {keys.extPubKey}";
+            AddressP2PKHLabel.Text = $"Address P2PKH: {keys.extKey.GetWif(NBitcoin.Network.Main).GetPublicKey().Hash.GetAddress(NBitcoin.Network.Main)}"; //BASE58 (P2PKH)
+            AddressSegwitLabel.Text = $"Address Segwit: {keys.extKey.GetWif(NBitcoin.Network.Main).GetPublicKey().WitHash.ScriptPubKey.GetDestinationAddress(NBitcoin.Network.Main)}"; //BECH32 (P2WPKH)
+            Console.WriteLine("Address P2PKH: " + AddressP2PKHLabel.Text);
+            Console.WriteLine("Address Segwit: " +AddressSegwitLabel.Text);
             //CounterLabel.Text = $"Current count: {count}";
         }
     }
