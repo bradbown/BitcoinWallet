3abaeeacef5e5c6d449573b240479a17f4d061bd
Bradley Bown <bradbown@googlemail.com>
2021-10-04 21:54:16 +0100

    Created MnemonicPage

---
+++ BitcoinWallet/MnemonicPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BitcoinWallet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MnemonicPage : ContentPage
+    {
+        public MnemonicPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
