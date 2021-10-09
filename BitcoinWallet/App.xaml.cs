2469e2e2dc154e828cba32bbb4154c7d9c674d91
Bradley Bown <bradbown@googlemail.com>
2021-10-09 13:32:33 +0100

    Set MainPage = new NavigationPage(new MainPage()); Navigation now working

--- BitcoinWallet/App.xaml.cs
+++ BitcoinWallet/App.xaml.cs
@@ -10,8 +10,7 @@ namespace BitcoinWallet
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
     }
 }
