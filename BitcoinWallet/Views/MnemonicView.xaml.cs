a081106cec8756d7d12295e1e8effdda3b6583e8
Bradley Bown <bradbown@googlemail.com>
2021-10-13 22:58:35 +0100

    Setup the mnemonic view page

--- BitcoinWallet/Views/MnemonicView.xaml.cs
+++ BitcoinWallet/Views/MnemonicView.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
 
 namespace BitcoinWallet.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MnemonicView : ContentView
     {
         public MnemonicView()
