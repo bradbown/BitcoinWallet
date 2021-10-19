981a81303feb99bfd47d70fe65d3915c1aa5b55e
Bradley Bown <bradbown@googlemail.com>
2021-10-19 19:59:40 +0100

    Created ViewModelBase

---
+++ BitcoinWallet/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BitcoinWallet.ViewModels
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
