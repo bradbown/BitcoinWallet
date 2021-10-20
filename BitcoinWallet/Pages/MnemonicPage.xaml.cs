c300a8c02157b3479eba67a9b3108b21b0cf7575
Bradley Bown <bradbown@googlemail.com>
2021-10-20 23:57:54 +0100

    Finally figured out how to pass the mnemonic data to my MnemonicPage, next step extrapolate to a model view to remove code from behind the page.

--- BitcoinWallet/Pages/MnemonicPage.xaml.cs
+++ BitcoinWallet/Pages/MnemonicPage.xaml.cs
@@ -2,14 +2,18 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
+using NBitcoin;
 
 namespace BitcoinWallet.Pages
 {
     public partial class MnemonicPage : ContentPage
     {
-        public MnemonicPage()
+        private Mnemonic mnemonic;
+        public MnemonicPage(Mnemonic mnemonic)
         {
             InitializeComponent();
+            this.mnemonic = mnemonic;
+            TestMnemonic.Text = mnemonic.ToString();
         }
     }
 }
