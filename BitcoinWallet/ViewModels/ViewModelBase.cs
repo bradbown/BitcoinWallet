0f87f682433c16181cc68d6478c7fdec01214b38
Bradley Bown <bradbown@googlemail.com>
2021-10-20 22:14:19 +0100

    Created ViewModelBase class to inheret BaseViewModel from mvvmhelpers

---
+++ BitcoinWallet/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinWallet.ViewModels
+{
+    internal class ViewModelBase : BaseViewModel
+    {
+    }
+}
