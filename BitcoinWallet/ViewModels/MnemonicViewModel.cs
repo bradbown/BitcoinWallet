609a3198b5e7b0d14af2ebed3b6756990b9957dc
Bradley Bown <bradbown@googlemail.com>
2021-10-20 22:25:41 +0100

    Setup MnemonicViewModel

--- BitcoinWallet/ViewModels/MnemonicViewModel.cs
+++ BitcoinWallet/ViewModels/MnemonicViewModel.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls;
 using NBitcoin;
 
 namespace BitcoinWallet.ViewModels
 {
-    internal class MnemonicViewModel : BindableObject
+    internal class MnemonicViewModel : ViewModelBase
     {
-        
+        public MnemonicViewModel()
+        {
+
+        }
     }
 }
