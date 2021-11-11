327251473f3ba9fb4a7e50c6355baf34260e719a
Bradley Bown <bradbown@googlemail.com>
2021-11-11 22:21:04 +0000

    Added bindings to back and next buttons, back button disabled on page 0

--- BitcoinWallet/ViewModels/MnemonicViewModel.cs
+++ BitcoinWallet/ViewModels/MnemonicViewModel.cs
@@ -6,7 +6,10 @@ namespace BitcoinWallet.ViewModels
 {
     internal class MnemonicViewModel : ViewModelBase
     {
+        int mnemonicPageCount = 0;
+
         Mnemonic mnemonicDisplay;
+        bool backEnabled = false;
 
         public MnemonicViewModel()
         {
@@ -20,21 +23,32 @@ namespace BitcoinWallet.ViewModels
             set => SetProperty(ref mnemonicDisplay, value); 
         }
 
+        public bool BackEnabled
+        {
+            get => backEnabled;
+            set => SetProperty(ref backEnabled, value);
+        }
         public void SetMnemonicDisplay(Mnemonic mnemonic)
         {
-            this.MnemonicDisplay = mnemonic;
+            MnemonicDisplay = mnemonic;
         }
 
         public ICommand OnBack { get; }
         public void Back()
         {
-
+            if(mnemonicPageCount > 0)
+            {
+                mnemonicPageCount--;
+                if (mnemonicPageCount == 0)
+                    BackEnabled = false;                
+            }
         }
 
         public ICommand OnNext { get; }
         public void Next()
         {
-
+            mnemonicPageCount++;
+            BackEnabled = true;
         }
     }
 }
