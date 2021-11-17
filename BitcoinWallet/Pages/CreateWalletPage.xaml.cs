b606cce630c908b2982e246f3328e4439e5556ff
Bradley Bown <bradbown@googlemail.com>
2021-11-17 23:30:34 +0000

    Created new page for use when creating a new wallet

---
+++ BitcoinWallet/Pages/CreateWalletPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace BitcoinWallet.Pages
+{
+    public partial class CreateWalletPage : ContentPage
+    {
+        public CreateWalletPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
