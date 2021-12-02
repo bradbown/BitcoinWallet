6368576fdf05281f78a276acc30e24c261629a37
Bradley Bown <bradbown@googlemail.com>
2021-12-02 21:23:47 +0000

    First commit for AvaloniaUI

---
+++ DraculaWallet/DraculaWallet/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraculaWallet.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
