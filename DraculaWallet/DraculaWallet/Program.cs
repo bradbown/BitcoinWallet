75d7a1ad091f54227cecfa603d37f5c0342197bb
Bradley Bown <bradbown@googlemail.com>
2021-12-06 18:22:58 +0000

    * Installed nuget package for FontAwesome Icons * Passing to RadioButton style through with tag

--- DraculaWallet/DraculaWallet/Program.cs
+++ DraculaWallet/DraculaWallet/Program.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 using System;
 
 namespace DraculaWallet
@@ -11,8 +13,13 @@ namespace DraculaWallet
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            // Register icon provider(s)
+            IconProvider.Register<FontAwesomeIconProvider>();
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
